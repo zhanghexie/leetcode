9f814a3ad78080d7dbc18d12e44c0d926ae95b5a
zhanghexuan <1479406235@qq.com>
2020-12-04 09:15:20 +0800

    更改代码格式为google格式

--- leetcode_547.cpp
+++ leetcode_547.cpp
@@ -1,28 +1,27 @@
-# include <vector>
+#include <vector>
 using namespace std;
 
 class Solution {
-public:
-    int findCircleNum(vector<vector<int>>& M) {
-        int num = 0;
-        // 把数组看成一张无向图，visited记录访问过的节点。
-        vector<bool> visited(M.size(), false);
-        for(int i = 0; i < M.size(); ++i){
-            if(!visited[i]){
-                dfs(M, i, visited);
-                ++num;
-            }
-        }
-        return num;
+ public:
+  int findCircleNum(vector<vector<int>>& M) {
+    int num = 0;
+    // 把数组看成一张无向图，visited记录访问过的节点。
+    vector<bool> visited(M.size(), false);
+    for (int i = 0; i < M.size(); ++i) {
+      if (!visited[i]) {
+        dfs(M, i, visited);
+        ++num;
+      }
     }
-    
-    void dfs(vector<vector<int>>& M, int n, vector<bool> &visited){
-        visited[n] = true;
-        for(int i = 0; i < visited.size(); ++i){
-            if(!visited[i] && 1 == M[i][n]){
-                dfs(M, i, visited);
-            }
-        }
-    }
-};
+    return num;
+  }
 
+  void dfs(vector<vector<int>>& M, int n, vector<bool>& visited) {
+    visited[n] = true;
+    for (int i = 0; i < visited.size(); ++i) {
+      if (!visited[i] && 1 == M[i][n]) {
+        dfs(M, i, visited);
+      }
+    }
+  }
+};
